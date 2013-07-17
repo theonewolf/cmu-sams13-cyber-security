ce9656f68103f7413160177657430641e980fca9
Wolfgang Richter <wolf@cs.cmu.edu>
2013-07-17 14:32:23 -0400

    had variables flipped

--- lectures/lecture12/pi_printer.py
+++ lectures/lecture12/pi_printer.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
         print USAGE % argv[0]
         exit(1)
 
-    offset = int(argv[1])
-    length = int(argv[2])
+    offset = int(argv[2])
+    length = int(argv[3])
 
     with open(argv[1]) as fd:
         data = fd.read()
