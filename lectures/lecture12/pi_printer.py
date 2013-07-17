3629846c5528b5ddeb70a8b2d5777e1cc464d88b
Wolfgang Richter <wolf@cs.cmu.edu>
2013-07-17 14:30:05 -0400

    lecture 12 initial version

---
+++ lectures/lecture12/pi_printer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+
+
+'''USAGE: %s <pi file> <offset> <length>'''
+
+
+
+if __name__ == '__main__':
+    if len(argv) < 4:
+        print USAGE % argv[0]
+        exit(1)
+
+    offset = int(argv[1])
+    length = int(argv[2])
+
+    with open(argv[1]) as fd:
+        data = fd.read()
+        print data[offset : offset + length]
